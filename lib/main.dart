179e36ce155039aa0afa167cc32df6dfa0f6ac11
nanang koesharwanto <nanangkoesharwanto@gmail.com>
2022-07-13 02:04:55 +0700

    three commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo BMI',
+      title: 'Flutter BMI',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo BMI Home Page'),
+      home: const MyHomePage(title: 'Flutter BMI Home'),
     );
   }
 }
